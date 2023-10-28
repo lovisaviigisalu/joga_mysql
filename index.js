0df7aa8b40b82f82ee4fb7cbe635a0ccee7531ff
Lovisa Viigisalu <l.viigisalu@gmail.com>
2023-10-28 20:33:18 +0300

    Displayed author article list

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ app.get('/',(req,res)=> {
         })
     })
 });
+
+
 //kuvab sellele artikli sisu
 app.get('/article/:slug',(req, res) =>{
     let query =`SELECT * FROM article WHERE slug="${req.params.slug}"`
@@ -66,6 +68,38 @@ app.get('/article/:slug',(req, res) =>{
         })
     })
 })
+
+
+//kui vajutada autori peale, siis tuleb ette selle autori artiklid
+app.get('/author/:author_id', (req, res) => {
+
+    let authorId = req.params.author_id;
+    //SQL-päring autorite andmete leidmiseks
+    let authorQuery = `SELECT * FROM author WHERE id=${authorId}`;
+    //SQL-päring autoriga seotud artikle leidmiseks
+    let articlesQuery = `SELECT * FROM article WHERE author_id=${authorId}`;
+
+    let author;
+    let articles;
+    //Päring autorite andmete leidmiseks
+    con.query(authorQuery, (err, authorResult) => {
+        if (err) throw err;
+        //Salvestab autorite andmed
+        author = authorResult[0];
+        //Päring autoriga seotud artiklite leidmiseks
+        con.query(articlesQuery, (err, articlesResult) => {
+            if (err) throw err;
+            //Salvestab artiklite andmed
+            articles = articlesResult;
+
+            //Renderdab autori malli andmetega
+            res.render('author', {
+                author: author,
+                articles: articles
+            });
+        });
+    });
+});
 app.listen(3000, () => {
     console.log(`App is started at http://localhost:3000`)
 })
